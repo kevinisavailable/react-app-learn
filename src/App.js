88f56b8ff5c00a7296b387c5db7f13608af15983
Aravind Roy <aravindroy1709@gmail.com>
2022-10-20 10:23:12 +0530

    Example 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+//This example shows how to pass props to components
+import Contact from "./example1";
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Contact name ="Student"/>
+      <Contact name = "Teacher"/> 
     </div>
   );
 }
