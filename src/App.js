981d502fc76314e21eee4364cfd58aecd5da46f3
Aravind Roy <aravindroy1709@gmail.com>
2022-10-21 10:22:40 +0530

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-
+import React from "react";
+import {BrowserRouter as Router, Routes, Route,Link} from "react-router-dom"
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Navbar from "./pages/Navbar";
 function App() {
   return (
     <div className="App">
+      <Router>
+        < Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={< Error />} />
+        </Routes>
+      </Router>
 
     </div>
   )
