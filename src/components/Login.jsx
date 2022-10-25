583c5dbc4899efc03b94fb372043de661c0fd103
Aravind Roy <aravindroy1709@gmail.com>
2022-10-25 10:28:27 +0530

    Example 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { login } from '../redux/userReducer'
+import { login,logout } from '../redux/userReducer'
 const Login = () => {
   const dispatch = useDispatch()
 
   return (
     <div>
         <button onClick={()=>{dispatch(login({name:"Pedro",age:20,email:"hello"}))}}>Login</button>
+        <button onClick={()=>{dispatch(logout())}}>Logout</button>
     </div>
   )
 }
