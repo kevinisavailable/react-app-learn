6cab7b6bdb3388df7b4ffec8b47920c3abe63612
Aravind Roy <aravindroy1709@gmail.com>
2022-10-25 07:06:19 +0530

    Installed Dependencies | Simple App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React from "react";
-import { CatFact } from "./catFact";
-import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
-import { Form } from "./Form";
-import { Counter } from "./Counter";
-function App() {
-const client = new QueryClient({
-  defaultOptions:{
-    queries:{
-      refetchOnWindowFocus:false
-    }
-  }
-})
-  return (
-    <div>
-      <QueryClientProvider client={client}>
-      {/* <CatFact /> */}
-      {/* <Form /> */}
-      <Counter />
-      </QueryClientProvider>
-   
-     </div>
-  );
+import "./app.css";
 
-}
+const App = () => {
+  return (
+    <>
+      <div className="container">
+      </div>
+    </>
+  );
+};
 
 export default App;
-
-
-
-
